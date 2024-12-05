b1bf95f38e701aeab7de0d0e17dcbc7ef9fc2e36
dahun1225 <ekgns153@naver.com>
2024-12-05 18:18:08 +0900

    프로세스 VO 추가

---
+++ src/main/java/dev/mvc/process/RecentRecomVO.java
@@ -0,0 +1,13 @@
+package dev.mvc.process;
+// RecentRecomVO.java
+public class RecentRecomVO {
+    private int recentRecomNo; // 최신 뉴스 기사번호
+    private int contentsNo; // 기사 번호
+
+    // Getters and Setters
+    public int getRecentRecomNo() { return recentRecomNo; }
+    public void setRecentRecomNo(int recentRecomNo) { this.recentRecomNo = recentRecomNo; }
+
+    public int getContentsNo() { return contentsNo; }
+    public void setContentsNo(int contentsNo) { this.contentsNo = contentsNo; }
+}
