7c9a1232ced6a4c88e331e3d51a010db6366bffe
hyeon6118 <lollol6118@gmail.com>
2024-12-11 16:51:10 +0900

    팀4 어플리케이션 수정

--- src/main/java/dev/mvc/team4/Team4V2sbm3cApplication.java
+++ src/main/java/dev/mvc/team4/Team4V2sbm3cApplication.java
@@ -1,14 +1,11 @@
 package dev.mvc.team4;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.mvc"}) // <-- 이 부분만 Copy and Paste
-@MapperScan("dev.mvc.genre")
-@MapperScan("dev.mvc.member")
 public class Team4V2sbm3cApplication {
 
     public static void main(String[] args) {
