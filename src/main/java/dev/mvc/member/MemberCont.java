c6c13b6eb8a9880e1af617a68691f3cd63c934d0
foqql <khk8734@naver.com>
2024-12-12 15:59:23 +0900

    로그인

--- src/main/java/dev/mvc/member/MemberCont.java
+++ src/main/java/dev/mvc/member/MemberCont.java
@@ -1,6 +1,7 @@
 package dev.mvc.member;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import dev.mvc.genre.GenreProcInter;
 import dev.mvc.genre.GenreVOMenu;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 @RequestMapping("/member")
@@ -62,5 +67,190 @@ public class MemberCont {
     
     return "/member/create";    // /template/member/create.html
   }
+  
+  @PostMapping(value="/create")
+  public String create_proc(Model model,
+                                     @ModelAttribute("memberVO") MemberVO memberVO) {
+    int checkID_cnt = this.memberProc.checkID(memberVO.getId());
+    
+    if (checkID_cnt == 0) {
+      memberVO.setGrade(15); // 기본 회원 15
+      int cnt = this.memberProc.create(memberVO);
+      
+      if (cnt == 1) {
+        model.addAttribute("code", "create_success");
+        model.addAttribute("mname", memberVO.getMname());
+        model.addAttribute("id", memberVO.getId());
+      } else {
+        model.addAttribute("code", "create_fail");
+      }
+      
+      model.addAttribute("cnt", cnt);
+    } else { // id 중복
+      model.addAttribute("code", "duplicte_fail");
+      model.addAttribute("cnt", 0);
+    }
+    
+    return "/member/msg"; // /templates/member/msg.html
+  }
+  
+  @GetMapping(value="/list")
+  public String list(Model model) {
+    ArrayList<MemberVO> list = this.memberProc.list();
+    
+    model.addAttribute("list", list);
+    
+    return "/member/list";  // /templates/member/list.html
+  }
+  
+  // ----------------------------------------------------------------------------------
+  // Cookie 사용 로그인 관련 코드 시작
+  // ----------------------------------------------------------------------------------
+  /**
+   * 로그인
+   * @param model
+   * @param memberno 회원 번호
+   * @return 회원 정보
+   */
+  @GetMapping(value="/login")
+  public String login_form(Model model, HttpServletRequest request) {
+    // Cookie 관련 코드---------------------------------------------------------
+    Cookie[] cookies = request.getCookies();
+    Cookie cookie = null;
+  
+    String ck_id = ""; // id 저장
+    String ck_id_save = ""; // id 저장 여부를 체크
+    String ck_passwd = ""; // passwd 저장
+    String ck_passwd_save = ""; // passwd 저장 여부를 체크
+  
+    if (cookies != null) { // 쿠키가 존재한다면
+      for (int i=0; i < cookies.length; i++){
+        cookie = cookies[i]; // 쿠키 객체 추출
+      
+        if (cookie.getName().equals("ck_id")){
+          ck_id = cookie.getValue();  // email
+        }else if(cookie.getName().equals("ck_id_save")){
+          ck_id_save = cookie.getValue();  // Y, N
+        }else if (cookie.getName().equals("ck_passwd")){
+          ck_passwd = cookie.getValue();         // 1234
+        }else if(cookie.getName().equals("ck_passwd_save")){
+          ck_passwd_save = cookie.getValue();  // Y, N
+        }
+      }
+    }
+    // ----------------------------------------------------------------------------
+    
+    //    <input type='text' class="form-control" name='id' id='id' 
+    //            th:value='${ck_id }' required="required" 
+    //            style='width: 30%;' placeholder="아이디" autofocus="autofocus">
+    model.addAttribute("ck_id", ck_id);
+  
+    //    <input type='checkbox' name='id_save' value='Y' 
+    //            th:checked="${ck_id_save == 'Y'}"> 저장
+    model.addAttribute("ck_id_save", ck_id_save);
+  
+    model.addAttribute("ck_passwd", ck_passwd);
+    model.addAttribute("ck_passwd_save", ck_passwd_save);
+
+//    model.addAttribute("ck_id_save", "Y");
+//    model.addAttribute("ck_passwd_save", "Y");
+    
+    return "/member/login_cookie";  // templates/member/login_cookie.html
+  }
+
+  /**
+   * Cookie 기반 로그인 처리
+   * @param session
+   * @param request
+   * @param response
+   * @param model
+   * @param id 아이디
+   * @param passwd 패스워드
+   * @param id_save 아이디 저장 여부
+   * @param passwd_save 패스워드 저장 여부
+   * @return
+   */
+  @PostMapping(value="/login")
+  public String login_proc(HttpSession session,
+                                     HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     Model model, 
+                                     @RequestParam(value="id", defaultValue = "") String id, 
+                                     @RequestParam(value="passwd", defaultValue = "") String passwd,
+                                     @RequestParam(value="id_save", defaultValue = "") String id_save,
+                                     @RequestParam(value="passwd_save", defaultValue = "") String passwd_save
+                                     ) {
+    HashMap<String, Object> map = new HashMap<String, Object>();
+    map.put("id", id);
+    map.put("passwd", passwd);
+    
+    int cnt = this.memberProc.login(map);
+    System.out.println("-> login_proc cnt: " + cnt);
+    
+    model.addAttribute("cnt", cnt);
+    
+    if (cnt == 1) {
+      // id를 이용하여 회원 정보 조회
+      MemberVO memverVO = this.memberProc.readById(id);
+      session.setAttribute("memberno", memverVO.getMemberno());
+      session.setAttribute("id", memverVO.getId());
+      session.setAttribute("mname", memverVO.getMname());
+      session.setAttribute("grade", memverVO.getGrade());
+      
+      // Cookie 관련 코드---------------------------------------------------------
+      // -------------------------------------------------------------------
+      // id 관련 쿠기 저장
+      // -------------------------------------------------------------------
+      if (id_save.equals("Y")) { // id를 저장할 경우, Checkbox를 체크한 경우
+        Cookie ck_id = new Cookie("ck_id", id);
+        ck_id.setPath("/");  // root 폴더에 쿠키를 기록함으로 모든 경로에서 쿠기 접근 가능
+        ck_id.setMaxAge(60 * 60 * 24 * 30); // 30 day, 초단위
+        response.addCookie(ck_id); // id 저장
+      } else { // N, id를 저장하지 않는 경우, Checkbox를 체크 해제한 경우
+        Cookie ck_id = new Cookie("ck_id", "");
+        ck_id.setPath("/");
+        ck_id.setMaxAge(0);
+        response.addCookie(ck_id); // id 저장
+      }
+      
+      // id를 저장할지 선택하는  CheckBox 체크 여부
+      Cookie ck_id_save = new Cookie("ck_id_save", id_save);
+      ck_id_save.setPath("/");
+      ck_id_save.setMaxAge(60 * 60 * 24 * 30); // 30 day
+      response.addCookie(ck_id_save);
+      // -------------------------------------------------------------------
+  
+      // -------------------------------------------------------------------
+      // Password 관련 쿠기 저장
+      // -------------------------------------------------------------------
+      if (passwd_save.equals("Y")) { // 패스워드 저장할 경우
+        Cookie ck_passwd = new Cookie("ck_passwd", passwd);
+        ck_passwd.setPath("/");
+        ck_passwd.setMaxAge(60 * 60 * 24 * 30); // 30 day
+        response.addCookie(ck_passwd);
+      } else { // N, 패스워드를 저장하지 않을 경우
+        Cookie ck_passwd = new Cookie("ck_passwd", "");
+        ck_passwd.setPath("/");
+        ck_passwd.setMaxAge(0);
+        response.addCookie(ck_passwd);
+      }
+      // passwd를 저장할지 선택하는  CheckBox 체크 여부
+      Cookie ck_passwd_save = new Cookie("ck_passwd_save", passwd_save);
+      ck_passwd_save.setPath("/");
+      ck_passwd_save.setMaxAge(60 * 60 * 24 * 30); // 30 day
+      response.addCookie(ck_passwd_save);
+      // -------------------------------------------------------------------
+      // ----------------------------------------------------------------------------     
+      
+      return "redirect:/";
+    } else {
+      model.addAttribute("code", "login_fail");
+      return "/member/msg";
+    }
+  }
+  
+  // ----------------------------------------------------------------------------------
+  // Cookie 사용 로그인 관련 코드 종료
+  // ----------------------------------------------------------------------------------
   
 }
