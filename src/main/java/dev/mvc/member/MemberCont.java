19c72a3a97bdb469af9c41bbba56c8920b392d9e
hyeon6118 <lollol6118@gmail.com>
2024-12-13 10:10:27 +0900

    member 오류

--- src/main/java/dev/mvc/member/MemberCont.java
+++ src/main/java/dev/mvc/member/MemberCont.java
@@ -10,21 +10,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-<<<<<<< HEAD
 import dev.mvc.classify.ClassifyProcInter;
 import dev.mvc.classify.ClassifyVOMenu;
-=======
-import dev.mvc.genre.GenreProcInter;
-import dev.mvc.genre.GenreVOMenu;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
->>>>>>> 562737c7e9669251cec8e55cda142d7a962c1d5f
 
 
 @RequestMapping("/member")
