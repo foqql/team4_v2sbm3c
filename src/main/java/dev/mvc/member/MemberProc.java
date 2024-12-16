c659b53cc17261a554f72608953a68080ea2888c
foqql <khk8734@naver.com>
2024-12-16 15:50:10 +0900

    사과해요

--- src/main/java/dev/mvc/member/MemberProc.java
+++ src/main/java/dev/mvc/member/MemberProc.java
@@ -49,41 +49,32 @@ public class MemberProc implements MemberProcInter {
     return memberVO;
   }
 
-@Override
+  @Override
   public boolean isMember(HttpSession session){
     boolean sw = false; // 로그인하지 않은 것으로 초기화
-    int grade = 99;
+    String grade = (String)session.getAttribute("grade");
     
-    // System.out.println("-> grade: " + session.getAttribute("grade"));
-    if (session != null) {
-      String id = (String)session.getAttribute("id");
-      if (session.getAttribute("grade") != null) {
-        grade = (int)session.getAttribute("grade");
-      }
-      
-      if (id != null && grade <= 20){ // 관리자 + 회원
+    if (grade != null) {
+      if (grade.equals("admin") || grade.equals("member")) {
         sw = true;  // 로그인 한 경우
-      }
+      }      
     }
     
     return sw;
   }
 
+  /**
+   * 관리자, 회원인지 검사
+   */  
   @Override
   public boolean isMemberAdmin(HttpSession session){
     boolean sw = false; // 로그인하지 않은 것으로 초기화
-    int grade = 99;
+    String grade = (String)session.getAttribute("grade");
     
-    // System.out.println("-> grade: " + session.getAttribute("grade"));
-    if (session != null) {
-      String id = (String)session.getAttribute("id");
-      if (session.getAttribute("grade") != null) {
-        grade = (int)session.getAttribute("grade");
-      }
-      
-      if (id != null && grade <= 10){ // 관리자 
+    if (grade != null) {
+      if (grade.equals("admin")) {
         sw = true;  // 로그인 한 경우
-      }
+      }      
     }
     
     return sw;
