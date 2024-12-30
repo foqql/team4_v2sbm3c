a7ca5a58a6dfa71c9de1546d438f7497e839fabb
foqql <khk8734@naver.com>
2024-12-30 12:27:04 +0900

    채팅창 완

--- src/main/java/dev/mvc/chat/ChatDAOInter.java
+++ src/main/java/dev/mvc/chat/ChatDAOInter.java
@@ -6,6 +6,5 @@ import java.util.Map;
 public interface ChatDAOInter {
     public int create(ChatVO chatVO); // 새 채팅 추가
     public List<ChatVO> list(); // 채팅 목록 조회
-
-
+    public int delete(int chatno); // 채팅 삭제
 }
