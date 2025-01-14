183576f255d254a1367fa5fbdb69fed0675ad300
hyeon6118 <lollol6118@gmail.com>
2025-01-14 19:22:48 +0900

    환율 서버 시작 크롤링

--- src/main/java/dev/mvc/news/startProc.java
+++ src/main/java/dev/mvc/news/startProc.java
@@ -12,36 +12,36 @@ import org.springframework.stereotype.Component;
 public class startProc implements ApplicationRunner  {
   @Override
   public void run(ApplicationArguments args) throws Exception {
-      System.out.println("111111111111111111111111111");
-      
-      
-//   // ------------------------------------------------------------------------------
-//      // Python 스크립트 실행 (추가된 부분)
-//      // ------------------------------------------------------------------------------
-//      try {
-//          String pythonScriptPath = "src/main/python/crawling.py"; // Python 스크립트 경로
-//          ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
-//          processBuilder.redirectErrorStream(true);
-//          Process process = processBuilder.start();
-//          System.out.println("22222222222222222222");
-//          // Python 스크립트의 출력 결과를 읽기
-//          BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//          String line;
-//          while ((line = reader.readLine()) != null) {
-//              System.out.println(line);
-//          }
+    System.out.println("-> 뉴스 크롤링 시작");
+    
+    
+ // ------------------------------------------------------------------------------
+    // Python 스크립트 실행 (추가된 부분)
+    // ------------------------------------------------------------------------------
+//    try {
+//        String pythonScriptPath = "src/main/python/crawling.py"; // Python 스크립트 경로
+//        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
+//        processBuilder.redirectErrorStream(true);
+//        Process process = processBuilder.start();
+//        System.out.println("-> 뉴스 크롤링 중");
+//        // Python 스크립트의 출력 결과를 읽기
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            System.out.println(line);
+//        }
 //
-//          int exitCode = process.waitFor();
-//          System.out.println("Python script finished with exit code: " + exitCode);
+//        int exitCode = process.waitFor();
+//        System.out.println("Python script finished with exit code: " + exitCode);
 //
-//          if (exitCode != 0) {
-//              System.out.println("Python script execution failed");
-//          }
-//      } catch (IOException | InterruptedException e) {
-//          e.printStackTrace();
-//      }
-//      
-//      System.out.println("33333333333333333");
+//        if (exitCode != 0) {
+//            System.out.println("Python script execution failed");
+//        }
+//    } catch (IOException | InterruptedException e) {
+//        e.printStackTrace();
+//    }
+//    
+//    System.out.println("-> 뉴스 크롤링 시작 끝");
       
   }
 }
